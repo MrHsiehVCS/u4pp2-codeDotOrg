bd1203e2af89ecfe7f8510571712a8770737bfab
Eric Hsieh <erich@villagechristian.org>
2022-11-17 00:05:33 -0800

    update main

--- src/main/java/u4pp/Main.java
+++ src/main/java/u4pp/Main.java
@@ -4,11 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // System.out.println("welcome to u4pp! Good luck :)\n"+
-        // "(Please delete this line of code before submitting :) )");
-
+        System.out.println("welcome to u4pp! Good luck :)\n"+
+        "(Please delete this line of code before submitting :) )");
     }
-    // TODO: Remove old tests & code from u4pp1 (leapyear, mymath)
-    // TODO: entire README (instructions, grading)
-    // TODO: finish a version of MonsterFighter.java
 }
